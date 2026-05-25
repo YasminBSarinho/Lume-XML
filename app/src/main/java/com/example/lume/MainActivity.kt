f08f0b988febd00b5289aed189de6a29f9524d7b
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2026-05-25 16:24:57 -0300

    feat: simplificando a navegação na mainActivit

--- app/src/main/java/com/example/lume/MainActivity.kt
+++ app/src/main/java/com/example/lume/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.lume
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.lume.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            exibirFragment(HomeFragment())
         }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_inicio -> exibirFragment(HomeFragment())
+                R.id.nav_livros -> exibirFragment(BibliotecaFragment())
+                R.id.nav_sortear_pink -> exibirFragment(SorteadorFragment())
+                R.id.nav_sortear_purple1 -> exibirFragment(MetasFragment())
+            }
+            true
+        }
+    }
+
+    private fun exibirFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
